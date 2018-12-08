1f6c2f02a5e2d125d1f8585e19fc6c7ccb171203
Beau Gunderson <beau@beaugunderson.com>
2018-12-08 15:12:36 -0800

    improve fake data generation

--- src/generate-data.ts
+++ src/generate-data.ts
@@ -1,45 +1,59 @@
 import faker from 'faker';
 import moment from 'moment';
+import { DOCTORS } from './doctors';
 import { INITIAL_VALUES, PatientEncounter } from './forms/PatientEncounterForm';
-import { sample, times } from 'lodash';
+import { interventions } from './patient-interventions';
+import { sample, sampleSize, times } from 'lodash';
 
 export function insertExamples(encounters: Nedb) {
   function insertExample() {
+    const sampledInterventions = sampleSize(interventions, 1 + Math.floor(Math.random() * 5));
+    const interventionValues = {};
+
+    for (const intervention of sampledInterventions) {
+      interventionValues[intervention.fieldName] = true;
+    }
+
     const doc: PatientEncounter = {
       ...INITIAL_VALUES,
-      patientName: faker.fake('{{name.lastName}}, {{name.firstName}}'),
+      ...interventionValues,
+      patientName: faker.fake('{{name.lastName}}, {{name.firstName}} {{name.firstName}}'),
       dateOfBirth: moment(faker.date.past()).format('YYYY-MM-DD'),
-      diagnosisFreeText: sample(['Colon cancer', 'Breast cancer', 'Finger cancer']) || '',
-      diagnosisStage: sample(['Early', 'Advanced']) || '',
+      diagnosisType: sample(['Malignant', 'Benign/Other', 'Unknown']),
+      diagnosisFreeText: sample(['Colon cancer', 'Breast cancer', 'Finger cancer']),
+      diagnosisStage: sample(['Early', 'Advanced']),
       encounterType: 'patient',
-      location:
-        sample(
-          ['Ballard', 'Cherry Hill', 'Edmonds', 'Issaquah', 'First Hill', 'True Cancer Center']
-        ) || '',
-      clinic:
-        sample(
-          [
-            'Breast Surgery',
-            'Colorectal Surgery',
-            'Gyn Onc',
-            'Head/Neck Surgery',
-            'Hematology',
-            'Inpatient',
-            'Ivy',
-            'Medical Oncology',
-            'Non-SCI MD',
-            'Palliative Care',
-            'Radiation Oncology',
-            'Radiosurgery',
-            'Thoracic Surgery'
-          ]
-        ) || '',
-      encounterDate: moment(faker.date.past()).format('YYYY-MM-DD'),
-      md: [sample(['Dr. A', 'Dr. B']) || ''],
-      mrn: `100${Math.floor(Math.random() * 100000)}`,
-      numberOfTasks: `${Math.floor(Math.random() * 5)}`,
-      research: sample([false, true]) || false,
-      timeSpent: `${Math.floor(Math.random() * 60)}`
+      location: sample([
+        'Ballard',
+        'Cherry Hill',
+        'Edmonds',
+        'Issaquah',
+        'First Hill',
+        'True Cancer Center'
+      ]),
+      clinic: sample([
+        'Breast Surgery',
+        'Colorectal Surgery',
+        'Gyn Onc',
+        'Head/Neck Surgery',
+        'Hematology',
+        'Inpatient',
+        'Ivy',
+        'Medical Oncology',
+        'Non-SCI MD',
+        'Palliative Care',
+        'Radiation Oncology',
+        'Radiosurgery',
+        'Thoracic Surgery'
+      ]),
+      encounterDate: moment(faker.date.recent(30)).format('YYYY-MM-DD'),
+      md: sampleSize(DOCTORS, Math.random() > 0.75 ? 2 : 1),
+      mrn: `100${Math.random()
+        .toString()
+        .slice(2, 9)}`,
+      numberOfTasks: `${1 + Math.floor(Math.random() * 5)}`,
+      research: sample([false, true]),
+      timeSpent: `${Math.ceil((Math.random() * 60) / 5) * 5}`
     };
 
     encounters.insert(doc);
