1518412b9c256ec82ab5f232c3fff0a8a59e5de6
Beau Gunderson <beau@beaugunderson.com>
2019-08-11 19:06:54 -0700

    fix a warning

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ import moment from 'moment';
 export const DATE_FORMAT_DATABASE = 'YYYY-MM-DD';
 export const DATE_FORMAT_DISPLAY = 'MM/DD/YYYY';
 
-export const FIRST_TRACKING_DATE = moment('11/01/2018');
+export const FIRST_TRACKING_DATE = moment('2018-11-01');
 export const OLDEST_POSSIBLE_AGE = 117;
